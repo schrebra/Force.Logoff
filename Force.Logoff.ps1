9ebd459ba3960c6dfe7fee754d1717fe8f1e0afe
schrebra <34587652+schrebra@users.noreply.github.com>
2022-08-29 19:36:34 -0700

    Update Force.Logoff.ps1

--- Force.Logoff.ps1
+++ Force.Logoff.ps1
@@ -1,1 +1,4 @@
-﻿(gwmi win32_operatingsystem).win32shutdown(4)
+#Forced Log Off (0 + 4) - Logs the user off the computer immediately and does not notify applications that the logon session is ending. This can result in a loss of data.
+
+(gwmi win32_operatingsystem).win32shutdown(4)
+
